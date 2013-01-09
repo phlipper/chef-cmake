ce588f799a695a4a37bd7e7d886ffbe582744d6d
Phil Cohen <github@phlippers.net>
2013-01-08 23:35:54 -0800

    bump version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Phil Cohen"
 maintainer_email  "github@phlippers.net"
 license           "MIT"
 description       "Install cmake"
-version           "0.1.0"
+version           "0.2.0"
 
 recipe "default", "Install default cmake support"
 

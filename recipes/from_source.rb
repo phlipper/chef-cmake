1d29f4b3abf99d427fcf25c72783a84cab1d62d1
odeits <odeits@gmail.com>
2014-07-02 15:59:08 -0700

    Create from_source.rb

---
+++ recipes/from_source.rb
@@ -0,0 +1,34 @@
+
+cmake_dirname = "cmake-#{node[:cmake][:version][:major]}.#{node[:cmake][:version][:minor]}.#{node[:cmake][:version][:fix]}.#{node[:cmake][:version][:build]}"
+cmake_filename = "#{cmake_dirname}.tar.gz"
+
+remote_file "#{Chef::Config[:file_cache_path]}/#{cmake_filename}" do
+  source "http://www.cmake.org/files/v#{node[:cmake][:version][:major]}.#{node[:cmake][:version][:minor]}/#{cmake_filename}"
+end
+
+execute "extract" do
+  command "tar xf #{Chef::Config[:file_cache_path]}/#{cmake_filename}"
+  cwd "#{Chef::Config[:file_cache_path]}"
+  notifies :run, "configure", "delayed"
+end
+
+execute "configure" do
+  command "./configure"
+  cwd "#{Chef::Config[:file_cache_path]}/#{cmake_dirname}"
+  action :nothing
+  notifies :run, 'make', :delayed
+end
+
+execute "make" do
+  command "make"
+  cwd "#{Chef::Config[:file_cache_path]}/#{cmake_dirname}"
+  action :nothing
+  notifies :run, 'make', :delayed
+end
+
+execute "make install" do
+  command "make install"
+  cwd "#{Chef::Config[:file_cache_path]}/#{cmake_dirname}"
+  action :nothing
+end
+  

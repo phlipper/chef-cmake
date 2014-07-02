2737d1cfdea403050d234ea131255832f17252c1
odeits <odeits@gmail.com>
2014-07-02 16:50:23 -0700

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default['cmake']['from_source'] = false
 default['cmake']['version']['major'] = 2
 default['cmake']['version']['minor'] = 8
 default['cmake']['version']['fix'] = 12
+default['cmake']['version']['build'] = 2

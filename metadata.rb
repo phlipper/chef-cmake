59740381d94a155c57826c16a44bbcc2d77a4f92
odeits <odeits@gmail.com>
2014-07-02 16:11:36 -0700

    bump version to 0.3.0

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Phil Cohen"
 maintainer_email  "github@phlippers.net"
 license           "MIT"
 description       "Install cmake"
-version           "0.2.0"
+version           "0.3.0"
 
 recipe "default", "Install default cmake support"
 

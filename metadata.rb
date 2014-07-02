4aa6991c794f41798937fbc0f857f28f9f35d2fe
odeits <odeits@gmail.com>
2014-07-02 16:53:45 -0700

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -7,6 +7,9 @@ version           "0.3.0"
 
 recipe "default", "Install default cmake support"
 
+%w{ build-essential }.each do |dependency|
+  depends dependency
+
 %w{ debian ubuntu redhat centos fedora }.each do |os|
   supports os
 end

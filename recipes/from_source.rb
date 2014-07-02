ec84ed3deebdfc48a4d6693c707edc099de65f9e
odeits <odeits@gmail.com>
2014-07-02 16:46:00 -0700

    Update from_source.rb

--- recipes/from_source.rb
+++ recipes/from_source.rb
@@ -4,32 +4,26 @@ cmake_filename = "#{cmake_dirname}.tar.gz"
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{cmake_filename}" do
   source "http://www.cmake.org/files/v#{node[:cmake][:version][:major]}.#{node[:cmake][:version][:minor]}/#{cmake_filename}"
+  action :create_if_missing
+  mode 0644
   notifies :run, "extract", :delayed
 end
 
-execute "extract" do
-  command "tar xf #{Chef::Config[:file_cache_path]}/#{cmake_filename}"
+execute "tar --no-same-owner -zxf #{Chef::Config[:file_cache_path]}/#{cmake_filename}" do
   cwd "#{Chef::Config[:file_cache_path]}"
-  notifies :run, "configure", :delayed
+  creates "#{Chef::Config[:file_cache_path]}/#{cmake_dirname}"
 end
 
 execute "configure" do
   command "./configure"
   cwd "#{Chef::Config[:file_cache_path]}/#{cmake_dirname}"
-  action :nothing
-  notifies :run, 'make', :delayed
 end
 
 execute "make" do
-  command "make"
   cwd "#{Chef::Config[:file_cache_path]}/#{cmake_dirname}"
-  action :nothing
-  notifies :run, 'make install', :delayed
 end
 
 execute "make install" do
-  command "make install"
   cwd "#{Chef::Config[:file_cache_path]}/#{cmake_dirname}"
-  action :nothing
 end
   

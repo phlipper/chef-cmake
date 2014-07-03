89e6109f32d49f5014a2c56472fcf9254917d3a2
odeits <odeits@gmail.com>
2014-07-02 17:06:12 -0700

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,13 +3,4 @@
 # Recipe:: default
 #
 
-if node[:cmake][:from_source] then
-  include_recipe "cmake::from_source"
-else
-  case node[:platform]
-  when "centos","fedora","redhat"
-    package "cmake28"
-  when "ubuntu","debian"
-    package "cmake"
-  end
-end
+include_recipe "cmake::from_#{node[:cmake][:install_method]}"

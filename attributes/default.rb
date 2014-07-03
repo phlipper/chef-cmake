13614aaca2c2805d0aa10ed6bf4f2099f082fbe2
odeits <odeits@gmail.com>
2014-07-02 17:03:38 -0700

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['cmake']['from_source'] = false
+default['cmake']['install_method'] = "package"
 default['cmake']['version']['major'] = 2
 default['cmake']['version']['minor'] = 8
 default['cmake']['version']['fix'] = 12
